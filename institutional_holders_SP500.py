3bf61da6a0f0151ee123d1c7fa9b72e8dd46a719
Olamylo <olaoyecharles1@gmail.com>
2024-01-20 17:59:34 +0100

    Added script to extract top 10 Institutional holders of S&P 500 companies

--- institutional_holders_SP500.py
+++ institutional_holders_SP500.py
@@ -1,13 +1,9 @@
 import yfinance as yf
 import pandas as pd
-
-
-import pandas as pd
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
-# engine = create_engine('mssql+pyodbc://Server_Name/Database_Name?driver=SQL+Server+Native+Client+11.0')
 
 def get_date(df, bulkid, date_added):
     df.insert(0, 'bulk_id', bulkid)
@@ -59,6 +55,17 @@ for symbol in symbols:
     except:
         print('error occured for ' + symbol + ' Moving on to next symbol')
         pass
+
+# filter by percentage ownership > 0.7
+df_1 = final_df[final_df['pctHeld'] > 0.07]
+
+df_1 = df_1.groupby(['symbol']).agg({'Holder': 'count', 'pctHeld': 'sum'})
+
+df_1 = df_1.reset_index()
+
+df_1 = df_1.sort_values(by=['pctHeld'], ascending=False)
+
+df_1.to_csv('./institution_top_owned_companies.csv', index=False, encoding="utf-8")
 
 final_df.to_csv('./stock_institutional_holders_SP500.csv', index=False, encoding="utf-8")
 
