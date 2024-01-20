f0ca743c115dc812bc1a16ed4fdb81e7d0085c0f
Olamylo <olaoyecharles1@gmail.com>
2024-01-20 18:23:37 +0100

    Update

--- institutional_holders_SP500.py
+++ institutional_holders_SP500.py
@@ -54,7 +54,7 @@ for symbol in symbols:
         print('error occured for ' + symbol + ' Moving on to next symbol')
         pass
 
-# filter by percentage ownership > 0.7
+# filter by percentage ownership > 7%
 df_1 = final_df[final_df['pctHeld'] > 0.07]
 
 df_1 = df_1.groupby(['symbol']).agg({'Holder': 'count', 'pctHeld': 'sum'})
@@ -63,7 +63,26 @@ df_1 = df_1.reset_index()
 
 df_1 = df_1.sort_values(by=['pctHeld'], ascending=False)
 
-df_1.to_csv('./institution_top_owned_companies.csv', index=False, encoding="utf-8")
+# df_1.to_csv('./institution_top_owned_companies.csv', index=False, encoding="utf-8")
+#
+
+
+end_date = datetime.now().strftime('%Y-%m-%d')
+start_date = datetime.now().strftime('%Y-%m-%d')
+
+#  loop through symbols in df_1, get stock price data for each symbol, create a new column in df_1 with the stock price data
+for index, row in df_1.iterrows():
+    print(row['symbol'])
+    sym = yf.Ticker(row['symbol'])
+
+    data = sym.history(start=start_date, end=end_date)
+    data = data.reset_index()
+    data = data[['Date', 'Close']]
+    data = data.rename(columns={'Close': 'stock_price'})
+
+    df_1.loc[index, 'stock_price'] = data['stock_price'][0]
+
+df_1.to_csv('./test_data.csv', index=False, encoding="utf-8")
 
 final_df.to_csv('./stock_institutional_holders_SP500.csv', index=False, encoding="utf-8")
 
